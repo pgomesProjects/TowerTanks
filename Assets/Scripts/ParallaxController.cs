144a41ecf62087a6a2ea641896e21740154bc65d
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-09-08 19:32:57 -0400

    Parallax Map, Multiple Cameras

---
+++ Assets/Scripts/ParallaxController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    [SerializeField] internal float parallaxSpeed;
+
+    private Vector2 backgroundSize;
+
+    //Tiles the width of the background to give it room to parallax
+    private float tileMultiplier = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundSize = GetComponent<SpriteRenderer>().bounds.size;
+        GetComponent<SpriteRenderer>().size = new Vector2(backgroundSize.x * tileMultiplier, backgroundSize.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Constantly move the background
+        transform.position += new Vector3(parallaxSpeed * Time.deltaTime, transform.position.y);
+
+        //If the background moves past the original background size, move the background back to its original position
+        if(transform.position.x < -backgroundSize.x)
+        {
+            transform.position = new Vector3(backgroundSize.x, transform.position.y, transform.position.z);
+        }
+    }
+}
