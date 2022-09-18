f623a783e237821b34e10ad3ca59f73896b6808c
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-09-17 21:32:22 -0400

    Test Layout Created, Ladder Movement Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,60 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float horizontal;
+    private Vector2 movement;
     private float speed = 8f;
     private Rigidbody2D rb;
     internal InteractableController currentInteractableItem;
+    [SerializeField] private float distance = 3f;
+    [SerializeField] private LayerMask ladderMask;
+    private bool isClimbing;
+    private float defaultGravity;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        defaultGravity = rb.gravityScale;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Move the player horizontally
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+
+        //Check to see if the player is colliding with the ladder
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, ladderMask);
+
+        //If the player is colliding with the ladder
+        if(hitInfo.collider != null)
+        {
+            //If the player is trying to move up, they are climbing
+            if(movement.y > 0.25f)
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        //If the player is climbing, move up and get rid of gravity temporarily
+        if (isClimbing)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, movement.y * speed);
+            rb.gravityScale = 0;
+        }
+        //Once the player stops climbing, bring back gravity
+        else
+        {
+            rb.gravityScale = defaultGravity;
+        }
     }
 
     //Send value from Move callback to the horizontal float
-    public void OnMove(InputAction.CallbackContext ctx) => horizontal = ctx.ReadValue<Vector2>().x;
+    public void OnMove(InputAction.CallbackContext ctx) => movement = ctx.ReadValue<Vector2>();
 
     public void OnInteract(InputAction.CallbackContext ctx)
     {
