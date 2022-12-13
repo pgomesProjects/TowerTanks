cb257b668d7e129e68fe20337117641efacbe2d3
Rerric <79161388+Rerric@users.noreply.github.com>
2022-12-13 03:03:02 -0500

    Setup Wheel with new Script for animation

--- Assets/WheelSpeed.cs
+++ Assets/WheelSpeed.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WheelSpeed : MonoBehaviour
 {
+    private float speed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class WheelSpeed : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        speed = (FindObjectOfType<PlayerTankController>().GetPlayerSpeed()) * 10f;
+        transform.Rotate(0f, 0f, speed * Time.deltaTime, Space.Self);
     }
 }
