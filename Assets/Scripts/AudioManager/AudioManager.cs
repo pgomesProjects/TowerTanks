2869bfdc7730cf1de053415da17afa47699c5e04
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2023-03-19 16:53:46 -0400

    Added Documentation For The AudioManager

--- Assets/Scripts/AudioManager/AudioManager.cs
+++ Assets/Scripts/AudioManager/AudioManager.cs
@@ -7,32 +7,14 @@ public class AudioManager : MonoBehaviour
 {
 
     public AudioMixerGroup bgmMixer, sfxMixer;
-    public Sound[] sounds;
+    [Tooltip("The list of sounds that will be played in the game.")] public Sound[] sounds;
 
-    void Awake()
-    {
-        foreach (Sound s in sounds)
-        {
-/*            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
-
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-
-            switch (s.soundType)
-            {
-                case SOUNDTYPE.BGM:
-                    if (sfxMixer != null)
-                        s.source.outputAudioMixerGroup = sfxMixer;
-                    break;
-                case SOUNDTYPE.SFX:
-                    if (bgmMixer != null)
-                        s.source.outputAudioMixerGroup = bgmMixer;
-                    break;
-            }*/
-        }
-    }
-
+    /// <summary>
+    /// Plays a sound using the Wwise Post event system.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="audioVol">The volume of the sound.</param>
+    /// <param name="audioLocation">The game object that the event will be played on.</param>
     public void Play(string name, float audioVol, GameObject audioLocation = null)
     {
         Sound s = GetSound(name);
@@ -40,20 +22,15 @@ public class AudioManager : MonoBehaviour
             s.audioEvent.Post(audioLocation);
         else
             s.audioEvent.Post(gameObject);
-        /*        s.source.Play();
-                s.source.volume = audioVol;*/
     }
 
-    public void PlayAtRandomPitch(string name, float audioVol)
-    {
-        Sound s = GetSound(name);
-/*        s.source.PlayOneShot(s.clip, audioVol);
-
-        float pitchRandom = UnityEngine.Random.Range(0.9f, 1.1f);
-
-        s.source.pitch = pitchRandom;*/
-    }
-
+    /// <summary>
+    /// Plays the section of a sound.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="audioVol">The volume of the sound.</param>
+    /// <param name="start">The start position for the sound to be played at (in seconds).</param>
+    /// <param name="stop">The end position for the sound to be played at (in seconds).</param>
     public void PlayAtSection(string name, float audioVol, float start, float stop)
     {
         Sound s = GetSound(name);
@@ -65,9 +42,9 @@ public class AudioManager : MonoBehaviour
         s.source.pitch = 1;*/
     }
 
-    private AudioClip MakeSubclip(AudioClip clip, float start, float stop)
+/*    private AudioClip MakeSubclip(AudioClip clip, float start, float stop)
     {
-        /*        int frequency = clip.frequency;
+        *//*        int frequency = clip.frequency;
                 int channels = clip.channels;
 
                 float timeLength = stop - start;
@@ -79,31 +56,32 @@ public class AudioManager : MonoBehaviour
                 clip.GetData(data, (int)(frequency * start));
                 newClip.SetData(data, 0);
 
-                return newClip;*/
+                return newClip;*//*
         return null;
-    }
-
-/*    public void PlayOneShot(string name, float audioVol)
-    {
-        Sound s = GetSound(name);
-        s.audioEvent.Post(gameObject);
-        //s.source.PlayOneShot(s.clip, audioVol);
     }*/
 
-    public bool IsPlaying(string name, GameObject playLocation = null) //Checks to see if the audio track is still playing
+    /// <summary>
+    /// Checks to see if an audio track is actively playing.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="playLocation">The game object that the Wwise event is attached to.</param>
+    /// <returns>If true, the sound searched for is playing. If false, the sound is not playing.</returns>
+    public bool IsPlaying(string name, GameObject playLocation = null)
     {
         Sound s = GetSound(name);
-
         uint[] playingIDs = new uint[sounds.Length];
 
+        //Get the ID of the event from the sound engine
         uint playingEventID = AkSoundEngine.GetIDFromString(s.audioEvent.Name);
         uint count = (uint)playingIDs.Length;
 
+        //Populate the playingIDs array with all events that are playing from the GameObject
         if(playLocation != null)
             AkSoundEngine.GetPlayingIDsFromGameObject(playLocation, ref count, playingIDs);
         else
             AkSoundEngine.GetPlayingIDsFromGameObject(gameObject, ref count, playingIDs);
 
+        //Check to see if the event ID being searched for is in the list of event IDs being played
         for (int i = 0; i < count; i++)
         {
             uint playingID = playingIDs[i];
@@ -115,69 +93,87 @@ public class AudioManager : MonoBehaviour
 
         return false;
 
-    }//end of isPlaying
+    }
 
+    /// <summary>
+    /// Changes the volume of a sound.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="audioVol">The new volume for the sound.</param>
     public void ChangeVolume(string name, float audioVol)
     {
         Sound s = GetSound(name);
-        //s.source.volume = audioVol;
     }
 
+    /// <summary>
+    /// Changes the pitch of a sound.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="audioPitch">The new pitch for the sound.</param>
     public void ChangePitch(string name, float audioPitch)
     {
         Sound s = GetSound(name);
-        //s.source.pitch = audioPitch;
     }
 
-    public void Pause(string name) //Pauses audio
+    /// <summary>
+    /// Pauses a sound event.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    public void Pause(string name)
     {
         Sound s = GetSound(name);
-        //s.source.Pause();
     }
 
+    /// <summary>
+    /// Suspends all active sound events.
+    /// </summary>
     public void PauseAllSounds()
     {
         AkSoundEngine.Suspend();
-/*        foreach (Sound s in sounds)
-        {
-            s.source.Pause();
-        }*/
     }
 
-    public void Resume(string name) //Resumes audio that was paused
+    /// <summary>
+    /// Resumes the sound event, if paused.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    public void Resume(string name)
     {
         Sound s = GetSound(name);
-        //s.source.UnPause();
     }
 
+    /// <summary>
+    /// Resumes all sounds events that are suspended.
+    /// </summary>
     public void ResumeAllSounds()
     {
         AkSoundEngine.WakeupFromSuspend();
-/*        foreach (Sound s in sounds)
-        {
-            s.source.UnPause();
-        }*/
     }
 
-    public void Stop(string name, GameObject audioLocation = null) //Stops a sound
+    /// <summary>
+    /// Stops a sound event.
+    /// </summary>
+    /// <param name="name">The name of the sound.</param>
+    /// <param name="audioLocation">The game object that the Wwise event is attached to.</param>
+    public void Stop(string name, GameObject audioLocation = null)
     {
         Sound s = GetSound(name);
         if(audioLocation != null)
             s.audioEvent.Stop(audioLocation);
         else
             s.audioEvent.Stop(gameObject);
-        //s.source.Stop();
     }
 
+    /// <summary>
+    /// Stops all active sound events.
+    /// </summary>
     public void StopAllSounds()
     {
         AkSoundEngine.StopAll();
-/*        foreach (Sound s in sounds)
-        {
-            s.source.Stop();
-        }*/
     }
 
+    /// <summary>
+    /// Updates the master volume of all sounds.
+    /// </summary>
     public void UpdateAllVolumes()
     {
 /*        foreach (Sound s in sounds)
@@ -196,5 +192,5 @@ public class AudioManager : MonoBehaviour
 
     private Sound GetSound(string name) => Array.Find(sounds, sound => sound.name == name);
     public string GetEventName(string name) => GetSound(name).audioEvent.Name;
-    public float GetSoundLength(string name) => 0f;//GetSound(name).clip.length;
+    public float GetSoundLength(string name) => 0f;
 }
