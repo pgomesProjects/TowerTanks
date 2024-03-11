e90315771388abe1e9db42064e3403a2b70cbc72
Ryan Pratt <rpratt5@student.fitchburgstate.edu>
2024-03-11 01:31:38 -0400

    Chunk Loader Terrain Testing

--- Assets/Scripts/Level/ChunkLoader.cs
+++ Assets/Scripts/Level/ChunkLoader.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ChunkLoader : MonoBehaviour
 {
-    private const float RENDER_DISTANCE = 300f;
+    private const float RENDER_DISTANCE = 600f;
 
     [SerializeField, Tooltip("The parent to keep all ground pieces in when spawned.")] private Transform groundParentTransform;
     [SerializeField, Tooltip("The chunk that the level starts with.")] private ChunkData startingChunk;
-    [SerializeField, Tooltip("The chunk prefab to use for spawning new chunks.")] private ChunkData chunkPrefab;
+    [SerializeField, Tooltip("The chunk prefabs to use for spawning new chunks.")] private ChunkData[] chunkPrefabs;
 
     private PlayerTankController playerTank;
 
@@ -21,31 +21,41 @@ public class ChunkLoader : MonoBehaviour
         playerTank = FindObjectOfType<PlayerTankController>();
 
         // Create and initialize the object pool
-        InitializeChunks();
+        InitializeChunks(1);
     }
 
     /// <summary>
-    /// Creates an object pool of chunks, starting with the starting chunk, and then places them.
+    /// Creates an object pool of chunks, starting with the starting chunk, and then places them. If directions is 1, chunks will only spawn to the right.
     /// </summary>
-    private void InitializeChunks()
+    private void InitializeChunks(int directions)
     {
         //Initialize starting chunk
         startingChunk.InitializeChunk(Vector3.zero);
         groundPool.Add(startingChunk);
 
         float direction = -1f;
+        if (directions == 1) direction = 1f;
         float chunkCounter = 0f;
+        float previousY = 0f;
 
-        //Creates each chunk and alternates between placing them to the right and the left of the world
+        //Creates each chunk in the world
         for (int i = 1; i < poolSize; i++)
         {
-            direction = -direction;
+            if (directions == 1) chunkCounter++; //if directions = 1, spawn only to the right
 
-            if(i % 2 == 1)
-                chunkCounter++;
+            if (directions == 2) //if going in both directions, alternate between left and right when spawning
+            {
+                direction = -direction;
 
-            ChunkData chunkData = InstantiateChunk(new Vector3(ChunkData.CHUNK_WIDTH * chunkCounter * direction, 0f, 0f));
+                if (i % 2 == 1)
+                    chunkCounter++;
+            }
+
+            ChunkData chunkData = InstantiateChunk(new Vector3(ChunkData.CHUNK_WIDTH * chunkCounter * direction, previousY, 0f));
             groundPool.Add(chunkData);
+
+            if (chunkData.chunkType == ChunkData.ChunkType.SLOPEUP) previousY += 23f;
+            else if (chunkData.chunkType == ChunkData.ChunkType.SLOPEDOWN) previousY -= 23f;
         }
     }
 
@@ -56,7 +66,8 @@ public class ChunkLoader : MonoBehaviour
     /// <returns>The data of the newly spawned chunk.</returns>
     private ChunkData InstantiateChunk(Vector3 spawnPosition)
     {
-        ChunkData newChunkTransform = Instantiate(chunkPrefab, spawnPosition, Quaternion.identity);
+        int random = Random.Range(0, chunkPrefabs.Length);
+        ChunkData newChunkTransform = Instantiate(chunkPrefabs[random], spawnPosition, Quaternion.identity);
         newChunkTransform.transform.SetParent(groundParentTransform);
         newChunkTransform.InitializeChunk(spawnPosition);
 
