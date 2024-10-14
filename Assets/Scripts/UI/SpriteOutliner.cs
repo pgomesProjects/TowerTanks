90f5d78477fb3395e79870104a232cd334a8270c
P. Gomes <70992905+pgomesProjects@users.noreply.github.com>
2024-10-14 19:50:18 -0400

    More Sprite Outline Work

--- Assets/Scripts/UI/SpriteOutliner.cs
+++ Assets/Scripts/UI/SpriteOutliner.cs
@@ -11,32 +11,54 @@ namespace TowerTanks.Scripts
     {
         [SerializeField, Tooltip("Enables / Disables the outline.")] private bool isVisible = false;
         [SerializeField, Tooltip("The width of the outline.")] private float outlineWidth;
-        [SerializeField, Tooltip("The smoothness of the outline.")] private int smoothness;
         [SerializeField, Tooltip("The color of the outline.")] private Color outlineColor;
         [SerializeField, Tooltip("The material for the outline.")] private Material outlineMat;
-        [SerializeField, Tooltip("The scale multiplier.")] private float scaleMultiplier = 1;
+        [SerializeField, Tooltip("The sorting order for the outline.")] private int sortingOrder = -1;
 
-        private List<LineRenderer> currentOutlines = new List<LineRenderer>();
+        [SerializeField, Tooltip("The scale multiplier for the LineRenderer.")] private float scaleMultiplier = 1f;
+        [SerializeField, Tooltip("The smoothness of the lines.")] private int smoothness;
 
-        [Button]
+        private List<GameObject> currentOutlines = new List<GameObject>();
+        private Vector3 center;
+
+        private bool currentVisibility;
+
+        [Button("Generate Outline")]
         public void DebugGenerateOutline()
         {
             ClearOutline();
+            center = CalculateBounds().center;
+            currentVisibility = isVisible;
 
             if (isVisible)
             {
                 //Generate an outline for all SpriteRenderers childed to the object
                 foreach (SpriteRenderer spriteRenderer in GetComponentsInChildren<SpriteRenderer>())
-                    GenerateOutline(spriteRenderer, smoothness);
+                    GenerateOutline(spriteRenderer);
+            }
+        }
+
+        [Button("Refresh")]
+        public void DebugRefreshOutline()
+        {
+            //If the visibility has changed, regenerate the outline
+            if (currentVisibility != isVisible)
+                DebugGenerateOutline();
+
+            //Adjust the color and thickness of the outline
+            foreach (GameObject outline in currentOutlines)
+            {
+                AdjustOutlineColor(outline);
+                AdjustOutlineThickness(outline);
             }
         }
 
         private void OnValidate()
         {
-            DebugGenerateOutline();
+            //DebugGenerateOutline();
         }
 
-        [Button]
+        [Button("Remove Outline")]
         public void DebugRemoveOutlines()
         {
             ClearOutline();
@@ -64,14 +86,13 @@ namespace TowerTanks.Scripts
         /// Generates a LineRenderer that circles around a SpriteRendere.
         /// </summary>
         /// <param name="spriteRenderer">The SpriteRenderer.</param>
-        /// <param name="numPointsBetween">The number of points between each edge (higher numbers used for smoothness).</param>
         public void GenerateOutline(SpriteRenderer spriteRenderer, int numPointsBetween = 0)
         {
             //If the SpriteRenderer or the sprite is null, return
             if (spriteRenderer == null || spriteRenderer.sprite == null)
                 return;
 
-            //Clamp the value of the points
+            //Clamp the number of points
             numPointsBetween = Mathf.Max(0, numPointsBetween);
 
             //Get the vertices from the sprite
@@ -113,6 +134,19 @@ namespace TowerTanks.Scripts
                     allVertices.Add(edge.Key, edge.Value);
             }
 
+            CreateLineRenderer(spriteRenderer, numPointsBetween, vertices, allVertices);
+            //CreateSpriteRenderer(spriteRenderer);
+        }
+
+        /// <summary>
+        /// Creates an outline of a SpriteRenderer using a LineRenderer.
+        /// </summary>
+        /// <param name="spriteRenderer">The SpriteRenderer to make the outline for.</param>
+        /// <param name="numPointsBetween">The number of points in between each vertex.</param>
+        /// <param name="vertices">A list of all vertices in the SpriteRenderer.</param>
+        /// <param name="allVertices">A dictionary of all unique vertices and their edge information.</param>
+        private void CreateLineRenderer(SpriteRenderer spriteRenderer, int numPointsBetween, Vector3[] vertices, Dictionary<int, int> allVertices)
+        {
             // Create line renderer
             LineRenderer currentOutline = new GameObject("Line").AddComponent<LineRenderer>();
 
@@ -122,23 +156,7 @@ namespace TowerTanks.Scripts
             currentOutline.transform.localEulerAngles = Vector3.zero;
             currentOutline.transform.localScale = Vector3.one;
             currentOutline.useWorldSpace = false;
-
-            Vector3 center = CalculateBounds().center;
-            Vector3 directionFromCenter = currentOutline.transform.position - center;
-            Vector3 scaledPosition = center + directionFromCenter * scaleMultiplier;
-            currentOutline.transform.position = scaledPosition;
-            currentOutline.transform.localScale *= scaleMultiplier;
-
-            //Add material, width, and color
-            currentOutline.material = outlineMat;
-            currentOutline.startWidth = currentOutline.endWidth = outlineWidth;
-            currentOutline.startColor = currentOutline.endColor = outlineColor;
-
-            //Draw behind the shape
-            currentOutline.sortingOrder = -1;
-
-            //Add to the list of current outlines for the object
-            currentOutlines.Add(currentOutline);
+            currentOutlines.Add(currentOutline.gameObject);
 
             // Loop through edge vertices in order
             int startingVertex = 0;
@@ -174,6 +192,84 @@ namespace TowerTanks.Scripts
             currentOutline.positionCount = outlinePositions.Count;
             for (int i = 0; i < outlinePositions.Count; i++)
                 currentOutline.SetPosition(i, outlinePositions[i]);
+
+            AdjustOutlineColor(currentOutline.gameObject);
+            AdjustOutlineThickness(currentOutline.gameObject);
+        }
+
+        /// <summary>
+        /// Creates a SpriteRenderer childed to the original with a color and thickness.
+        /// </summary>
+        /// <param name="spriteRenderer">The SpriteRenderer to create an outline for.</param>
+        private void CreateSpriteRenderer(SpriteRenderer spriteRenderer)
+        {
+            GameObject newOutline = Instantiate(spriteRenderer.gameObject, spriteRenderer.transform);
+            newOutline.name = "Outline";
+            newOutline.transform.localPosition = Vector3.zero;
+            newOutline.transform.localEulerAngles = Vector3.zero;
+            newOutline.transform.localScale = Vector3.one;
+            currentOutlines.Add(newOutline);
+
+            AdjustOutlineColor(newOutline);
+            AdjustOutlineThickness(newOutline);
+        }
+
+        /// <summary>
+        /// Adjusts the color of the outline.
+        /// </summary>
+        /// <param name="outline">The current outline GameObject.</param>
+        private void AdjustOutlineColor(GameObject outline)
+        {
+            //Adjust the color and the sorting order
+            SpriteRenderer spriteRenderer;
+            if(outline.TryGetComponent(out spriteRenderer))
+            {
+                spriteRenderer.color = outlineColor;
+                spriteRenderer.material = outlineMat;
+                spriteRenderer.sortingOrder = sortingOrder;
+            }
+
+            //Adjust the material, color, and sorting order
+            LineRenderer outlineLineRenderer;
+            if(outline.TryGetComponent(out outlineLineRenderer))
+            {
+                outlineLineRenderer.material = outlineMat;
+                outlineLineRenderer.startColor = outlineLineRenderer.endColor = outlineColor;
+                outlineLineRenderer.sortingOrder = sortingOrder;
+
+            }
+        }
+
+        /// <summary>
+        /// Adjust the thickness of the outline.
+        /// </summary>
+        /// <param name="outline">The current outline GameObject.</param>
+        private void AdjustOutlineThickness(GameObject outline)
+        {
+            outline.transform.localPosition = Vector3.zero;
+
+            SpriteRenderer spriteRenderer;
+            if(outline.TryGetComponent(out spriteRenderer))
+            {
+                SpriteRenderer parentRenderer = spriteRenderer.transform.parent.GetComponent<SpriteRenderer>();
+
+                Vector2 parentSize = parentRenderer.transform.localScale;
+                float extraWorldUnits = outlineWidth / parentRenderer.sprite.pixelsPerUnit;
+
+                Vector2 targetSize = new Vector2(parentSize.x + 2 * extraWorldUnits,parentSize.y + 2 * extraWorldUnits);
+                spriteRenderer.transform.localScale = new Vector3(targetSize.x / parentSize.x, targetSize.y / parentSize.y, spriteRenderer.transform.localScale.z);
+            }
+
+            LineRenderer outlineLineRenderer;
+            if (outline.TryGetComponent(out outlineLineRenderer))
+            {
+                //Adjust the position and scale based on the multiplier
+                Vector3 directionFromCenter = outline.transform.position - center;
+                Vector3 scaledPosition = center + directionFromCenter * scaleMultiplier;
+                outline.transform.position = scaledPosition;
+                outline.transform.localScale = Vector3.one * scaleMultiplier;
+                outlineLineRenderer.startWidth = outlineLineRenderer.endWidth = outlineWidth;
+            }
         }
 
         /// <summary>
@@ -181,14 +277,14 @@ namespace TowerTanks.Scripts
         /// </summary>
         private void ClearOutline()
         {
-            foreach(LineRenderer line in currentOutlines)
+            foreach(GameObject outline in currentOutlines)
             {
-                if (line != null)
+                if (outline != null)
                 {
 #if UNITY_EDITOR
-                    DestroyImmediate(line.gameObject);
+                    DestroyImmediate(outline);
 #else
-                    Destroy(line.gameObject);
+                    Destroy(outline);
 #endif
                 }
             }
