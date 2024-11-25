e859bded9b342dfc96977c26e3b3e737eaee6d92
Keegan77 <keegansb77@gmail.com>
2024-11-25 18:09:26 -0500

    Fixed AI mortar aiming oversights and bugs (mortars are scary now)
    Fixed bug where mortar wouldn't shoot at the correct velocity the AI decided on, and fixed oversight that broke mortars from previous bug fix

---
+++ Assets/Scripts/EnemyScripts/WeaponBrain.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace TowerTanks.Scripts
+{
+    public class WeaponBrain : InteractableBrain
+    {
+        internal GunController gunScript;
+        
+        public float fireCooldown;
+        internal float fireTimer;
+        public float aimCooldown;
+        internal float aimTimer;
+        public float cooldownOffset;
+
+        public bool isRotating;
+        internal float currentForce;
+        internal List<Vector3> trajectoryPoints;
+        internal Vector3 hitPoint;
+
+        private void Awake()
+        {
+            gunScript = GetComponent<GunController>();
+        }
+
+        protected void Start()
+        {
+            fireTimer = 0;
+            aimTimer = 0;
+        }
+
+        // Update is called once per frame
+        protected void Update()
+        {
+            if (fireTimer < fireCooldown) fireTimer += Time.deltaTime;
+            else
+            {
+                gunScript.Fire(false, gunScript.tank.tankType);
+                float randomOffset = Random.Range(-cooldownOffset, cooldownOffset);
+                fireTimer = 0 + randomOffset;
+            }
+
+            if (aimTimer < aimCooldown) aimTimer += Time.deltaTime;
+            else
+            {
+                float randomForce = Random.Range(-1.2f, 1.2f);
+                //StartCoroutine(AimCannon(randomForce));
+
+                float randomOffset = Random.Range(-2f, 2f);
+                aimTimer = 0 + randomOffset;
+            }
+            AimAtTarget();
+            gunScript.RotateBarrel(currentForce, false);
+            
+        }
+        
+        public virtual void AimAtTarget() //mortar overrides bc it uses slightly different aiming
+        {
+            if (myTankAI.targetTank == null) return;
+
+            var proj = gunScript.projectilePrefab.GetComponent<Projectile>();
+
+            trajectoryPoints = Trajectory.GetTrajectory(gunScript.barrel.position, gunScript.barrel.right * gunScript.muzzleVelocity, proj.gravity, 100);
+            hitPoint = Trajectory.GetHitPoint(trajectoryPoints);
+
+            if (hitPoint == trajectoryPoints[^1]) // this means the trajectory hit nothing at all. so we want to use a
+            {                                      // point just a bit ahead of the barrel for accurate aiming,
+                hitPoint = trajectoryPoints[10];   // instead of that default endpoint
+            }
+            
+            //Adding Vector3.up * 2.5f because the tread system's transform is a bit low, we want to aim a little above that
+            Vector3 target = myTankAI.targetTank.treadSystem.transform.position + Vector3.up * 2.5f;
+            
+            Vector3 diff = hitPoint - target;
+            
+            
+            currentForce = diff.y > 0 ? 1 : -1;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (trajectoryPoints == null) return;
+            for (int i = 0; i < trajectoryPoints.Count - 1; i++)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(trajectoryPoints[i], trajectoryPoints[i + 1]);
+                if (trajectoryPoints[i] == hitPoint) break; // stops projecting line at target
+            }
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(hitPoint, 0.5f);
+            
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(myTankAI.targetTank.treadSystem.transform.position + Vector3.up * 2.5f, 1f);
+            
+        }
+    }
+}
--- Assets/Scripts/TankAI/TankAI.cs
+++ Assets/Scripts/TankAI/TankAI.cs
@@ -1,47 +1,88 @@
 using System;
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
+using System.Drawing;
+using UnityEditor;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Color = UnityEngine.Color;
+using Random = UnityEngine.Random;
 
 namespace TowerTanks.Scripts
 {
     public class TankAI : MonoBehaviour
     {
-        private StateMachine _stateMachine;
-        private TankController _tank, targetTank;
+        #region Transition Conditions
+        public bool TargetInViewRange() =>      Vector2.Distance(tank.treadSystem.transform.position, _tankManager.playerTank.treadSystem.transform.position) < aiSettings.viewRange;
+        public bool TargetOutOfView() =>        Vector2.Distance(tank.treadSystem.transform.position, _tankManager.playerTank.treadSystem.transform.position) > aiSettings.viewRange;
+        public bool TargetInEngageRange() =>    Vector2.Distance(tank.treadSystem.transform.position, _tankManager.playerTank.treadSystem.transform.position) < aiSettings.maxEngagementRange;
+        public bool TargetOutOfEngageRange() => Vector2.Distance(tank.treadSystem.transform.position, _tankManager.playerTank.treadSystem.transform.position) > aiSettings.maxEngagementRange;
+        public bool TargetTooClose() =>         Vector2.Distance(tank.treadSystem.transform.position, _tankManager.playerTank.treadSystem.transform.position) < aiSettings.minEngagementRange;
+        bool NoGuns() => !tank.interactableList.Any(i => i.script is GunController);
+        #endregion
+        
+        public static Dictionary<INTERACTABLE, Type> interactableEnumToBrainMap = new()
+        {
+            {INTERACTABLE.Cannon, typeof(SimpleCannonBrain)},
+            {INTERACTABLE.Mortar, typeof(SimpleMortarBrain)},
+            {INTERACTABLE.MachineGun, typeof(SimpleMachineGunBrain)},
+            {INTERACTABLE.Throttle, typeof(InteractableBrain)},
+        };
+        
+        private StateMachine fsm;
+        [FormerlySerializedAs("_tank")] [HideInInspector] public TankController tank;
+        [HideInInspector] public TankController targetTank;
         private TankManager _tankManager;
         private GunController[] _guns;
-        private float currentTokenCount;
+        private int currentTokenCount;
+        [HideInInspector] public List<InteractableId> tokenActivatedInteractables = new List<InteractableId>();
+        private Vector3 movePoint;
 
         [Header("AI Configuration")]
         public TankAISettings aiSettings;
 
         private void Awake()
         {
-            _tank = GetComponent<TankController>();
-            if (_tank.tankType == TankId.TankType.PLAYER)
-            {
-                Destroy(this);
-                return;
-            }
+            tank = GetComponent<TankController>();
+            
             currentTokenCount = aiSettings.tankEconomy;
             _tankManager = FindObjectOfType<TankManager>();
-            _stateMachine = new StateMachine();
+        }
+        
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(0.1f); // AI Waits before initialization to give time for any tank room generation
 
+            fsm = new StateMachine();
             var patrolState = new TankPatrolState(this);
             var pursueState = new TankPursueState(this);
             var engageState = new TankEngageState(this);
             var surrenderState = new TankSurrenderState(this);
+            
+            
+            //bool condition if there are no guncontrollers in _tank
+            
+            
+            void At(IState from, IState to, Func<bool> condition) => fsm.AddTransition(from, to, condition);
+            void AnyAt(IState to, Func<bool> condition) => fsm.AddAnyTransition(to, condition);
+            
+            //patrol state transitions
+            At(patrolState, pursueState, TargetInViewRange);
+            At(pursueState, patrolState ,TargetOutOfView);
+            //pursue state transitions
+            At(pursueState, engageState ,TargetInEngageRange);
+            At(engageState, pursueState ,TargetOutOfEngageRange);
+            AnyAt(surrenderState, NoGuns); //this being an "any transition" means that it can be triggered from any state
+            
+            fsm.SetState(patrolState);
+        }
 
-            void At(IState to, IState from, Func<bool> condition) => _stateMachine.AddTransition(to, from, condition);
-
-            bool PlayerInViewRange() => Vector2.Distance(_tank.transform.position, _tankManager.playerTank.transform.position) < aiSettings.viewRange;
-            bool TargetInEngagementRange() => Vector2.Distance(_tank.transform.position, targetTank.transform.position) < aiSettings.engagementRange;
-
-            At(pursueState, patrolState, PlayerInViewRange);
-            At(engageState, pursueState, TargetInEngagementRange);
-            _stateMachine.SetState(patrolState);
+        public bool HasActiveThrottle()
+        {
+            //see if token activated interactables has a throttle interactable in it
+            return tokenActivatedInteractables.Any(i => i.brain.GetType() == interactableEnumToBrainMap[INTERACTABLE.Throttle]);
         }
 
         public void SetTarget(TankController tank)
@@ -56,13 +97,224 @@ namespace TowerTanks.Scripts
 
         private void Update()
         {
-            _stateMachine.FrameUpdate();
-            var tanks = Physics2D.OverlapCircleAll(transform.position, aiSettings.viewRange, 1 << LayerMask.NameToLayer("Treads"));
+            if (fsm == null) return;
+            fsm.FrameUpdate();
+            //var tanks = Physics2D.OverlapCircleAll(transform.position, aiSettings.viewRange, 1 << LayerMask.NameToLayer("Treads"));
         }
 
         private void FixedUpdate()
         {
-            _stateMachine.PhysicsUpdate();
+            if (fsm == null) return;
+            fsm.PhysicsUpdate();
+        }
+
+        #region Tank AI Token System
+        
+        public void DistributeToken(INTERACTABLE toInteractable)
+        {
+            if (currentTokenCount <= 0) return;
+            
+            List<InteractableId> commonInteractables = tank.interactableList
+                .Where(i => i.brain != null && i.brain.GetType() == interactableEnumToBrainMap[toInteractable]
+                && !tokenActivatedInteractables.Contains(i))
+                .ToList();
+            //list of all interactables which have an AI, as long as the AI is registered to an enum type in the dict,
+            //and the interactable isn't already active
+
+            if (!commonInteractables.Any()) return; //if there are no interactables of this type, return
+            InteractableId interactable = commonInteractables[Random.Range(0, commonInteractables.Count)];
+            
+            if (interactable != null)
+            {
+                tokenActivatedInteractables.Add(interactable);
+                interactable.ReceiveToken(this);
+                interactable.brain.myInteractableType = toInteractable; //nice way to tell the brain what it is automatically
+                currentTokenCount--;
+            }
+            else
+            {
+                Debug.LogError("Could not distribute token: Interactable already in use, or not found");
+            }
+            
+        }
+
+        public void RetrieveToken(InteractableId interactableToTakeFrom)
+        {
+            if (tokenActivatedInteractables.Contains(interactableToTakeFrom))
+            {
+                tokenActivatedInteractables.Remove(interactableToTakeFrom);
+                interactableToTakeFrom.ReturnToken();
+                currentTokenCount++;
+            }
+            
+        }
+        
+        public void RetrieveAllTokens()
+        {
+            // Create a temporary list to store interactables to be removed.
+            // This avoids removing elements from the same list that we are iterating over,
+            // which would cause an exception.
+            List<InteractableId> interactablesToRemove = new List<InteractableId>(tokenActivatedInteractables);
+
+            // Iterate over the temporary list and remove interactables from the original list
+            foreach (var interactable in interactablesToRemove)
+            {
+                RetrieveToken(interactable);
+            }
+            
+        }
+        
+        public void DistributeAllWeightedTokens(Dictionary<INTERACTABLE, float> weights)
+        {
+            float totalWeight = weights.Values.Sum();
+            
+            // scales weights to add up to 100%
+            // (if they are under 100% it just won't distribute 100% of the tank's tokens)
+            if (totalWeight > 100)
+            {
+                float ratio = 100 / totalWeight;
+                
+                foreach (var key in weights.Keys.ToList())
+                {
+                    weights[key] *= ratio;
+                }
+            }
+            
+            // Calculate tokens for each type based on weight percentage
+            Dictionary<INTERACTABLE, int> tokensToDistribute = new Dictionary<INTERACTABLE, int>();
+            foreach (var weight in weights)
+            {
+                int tokens = Mathf.RoundToInt((weight.Value / 100) * currentTokenCount);
+                tokensToDistribute[weight.Key] = tokens;
+                if (weight.Key == INTERACTABLE.Throttle && tokens > 0) tokensToDistribute[weight.Key] = 1; //doesn't make sense to distribute more than 1 throttle token
+                Debug.Log($"Distributing {tokens} tokens to {weight.Key}");
+            }
+            
+            int totalTokensToDistribute = tokensToDistribute.Values.Sum();
+
+            //#1#/ if we have too many tokens to distribute, take away from the type with the most weight
+            while (totalTokensToDistribute > currentTokenCount)
+            { 
+                var maxWeightType = tokensToDistribute.OrderByDescending(kvp => weights[kvp.Key]).First().Key; //orders the key-value pairs (kvp) in the tokensToDistribute dictionary in descending order based on the weight values from the weights dictionary. The kvp.Key is used to access the corresponding weight in the weights dictionary.
+                tokensToDistribute[maxWeightType]--;
+                totalTokensToDistribute--;
+                Debug.Log($"Extra token was generated, removing extra token from {maxWeightType}");
+            }
+
+
+            foreach (var tokenDistribution in tokensToDistribute)
+            {
+                for (int i = 0; i < tokenDistribution.Value; i++)
+                {
+                    DistributeToken(tokenDistribution.Key);
+                }
+            }
+            
+        }
+
+
+        #endregion
+        
+        
+        #region Tank AI Functionality Methods
+        
+        /// <summary>
+        /// Will set the tank's throttle towards it's set movepoint value.
+        /// </summary>
+        /// <param name="speedSetting">
+        /// The speed setting to set the throttle to.
+        /// </param>
+        public void ChangeThrottleTowardsMovepoint(int speedSetting)
+        {
+            int dir = tank.transform.position.x < movePoint.x ? 1 : -1;
+            tank.SetTankGear(speedSetting, .1f);
+        }
+        
+        public void SetMovePoint(Vector3 point)
+        {
+            movePoint = point;
+        }
+        
+        public List<InteractableId> GetUnusedInteractables()
+        {
+            return tank.interactableList
+                .Where(i => !tokenActivatedInteractables.Contains(i))
+                .ToList();
+        }
+        
+        public bool TankIsRightOfTarget()
+        {
+            return tank.treadSystem.transform.position.x > targetTank.treadSystem.transform.position.x;
+        }
+
+        /// <summary>
+        /// Will randomly set the tank's throttle to go left or right.
+        /// </summary>
+        /// <param name="speedSetting">
+        /// The speed setting to set the throttle to.
+        /// </param>
+        public void MoveRandom(int speedSetting)
+        {
+            int s = Mathf.Abs(speedSetting);
+            if (s == 0)
+            {
+                Debug.LogError("Speed setting cannot be 0 in the MoveRandom Function");
+                return;
+            }
+            
+            if (Random.Range(0, 2) == 1)
+            {
+                tank.SetTankGear(s, .15f);
+            }
+            else
+            {
+                tank.SetTankGear(-s, .15f);
+            }
+        }
+
+        #endregion
+
+
+        private void OnDrawGizmos()
+        {
+            //draw circle for view range
+            if (!enabled) return;
+            Gizmos.color = Color.red;
+            if (tank == null) return;
+            Vector3 tankPos = tank.treadSystem.transform.position;
+            Gizmos.DrawWireSphere(tankPos, aiSettings.viewRange);
+            int i = 0;
+            foreach (var interactable in tokenActivatedInteractables)
+            {
+                //draw a box for each of their bounds
+                Gizmos.color = Color.blue;
+                Bounds bnds = interactable.script.thisCollider.bounds;
+                Gizmos.DrawWireCube(interactable.script.transform.position, bnds.size);
+                GUIStyle style = new GUIStyle();
+                style.fontSize = 20; // Set the desired font size
+                style.fontStyle = FontStyle.Bold;
+                style.normal.textColor = Color.green;
+                Handles.Label(interactable.script.transform.position + Vector3.up * 2, $"{i}", style:style);
+                style.normal.textColor = Color.cyan;
+                Handles.Label(tank.treadSystem.transform.position + Vector3.up * 25, $"AI STATE: {fsm._currentState.GetType().Name}", style:style);
+                style.normal.textColor = Color.yellow;
+                Handles.Label(tank.treadSystem.transform.position + Vector3.up * 22, $"Available Tokens: {currentTokenCount}", style:style);
+                style.normal.textColor = Color.red;
+                Handles.Label(tank.treadSystem.transform.position + Vector3.up * 19, $"Total Tokens: {aiSettings.tankEconomy}", style:style);
+                i++;
+            }
+            
+            //draw circle for engagement range
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(tankPos, aiSettings.maxEngagementRange);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(tankPos, aiSettings.minEngagementRange);
+        }
+
+        private void OnDestroy()
+        {
+            StackTrace stackTrace = new StackTrace(true);
+            UnityEngine.Debug.Log($"Component {GetType().Name} on GameObject {gameObject.name} is being destroyed. Stack trace:\n{stackTrace}", this);
         }
     }
 }
