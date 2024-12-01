a9e1af5ce008bf6799271a08be131534d10d157d
Ryan Pratt <cryanpratt@gmail.com>
2024-12-01 14:45:45 -0500

    Revert "Merge branch 'PGomes/dev-main-branch' into proto-ryan-branch"
    This reverts commit b30c73f37ae171781fbdc811bbe3e80956146283, reversing
    changes made to 7972ecc187e459bbcafeebb8ef0c7bf8ff830343.

--- Assets/Scripts/Analytics/AnalyticsManager.cs
+++ Assets/Scripts/Analytics/AnalyticsManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TowerTanks.Scripts
@@ -6,33 +8,26 @@ namespace TowerTanks.Scripts
     public class GameAnalytics
     {
         public float timePlayed { get; private set; }
-        public string gameStartTime { get; private set; }
 
         public GameAnalytics()
         {
-            timePlayed = 0f;
-            gameStartTime = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            this.timePlayed = 0f;
         }
 
         public void AddToTimePlayed(float timeElapsed) => timePlayed += timeElapsed;
-
-        public string PrintTimePlayed()
-        {
-            int hours = Mathf.FloorToInt(timePlayed / 3600);
-            int minutes = Mathf.FloorToInt(timePlayed % 3600 / 60);
-            int seconds = Mathf.FloorToInt(timePlayed % 60);
-
-            if (hours > 0)
-                return string.Format("Time Played: {0:0}:{1:00}:{2:00}", hours, minutes, seconds);
-            else
-                return string.Format("Time Played: {0:0}:{1:00}", minutes, seconds);
-        }
 
         public override string ToString()
         {
             string data = "";
 
-            data += PrintTimePlayed();
+            int hours = Mathf.FloorToInt(timePlayed / 3600);
+            int minutes = Mathf.FloorToInt(timePlayed % 3600 / 60);
+            int seconds = Mathf.FloorToInt(timePlayed % 60);
+
+            if (hours > 0)
+                data += string.Format("Time Played: {0:0}:{1:00}:{2:00}", hours, minutes, seconds);
+            else
+                data += string.Format("Time Played: {0:0}:{1:00}", minutes, seconds);
 
             return data;
         }
@@ -41,8 +36,7 @@ namespace TowerTanks.Scripts
 
     public class AnalyticsManager : MonoBehaviour
     {
-        public static GameAnalytics currentGameAnalytics { get; private set; }
-
+        private GameAnalytics currentGameAnalytics;
         private void Awake()
         {
             currentGameAnalytics = new GameAnalytics();
@@ -55,7 +49,7 @@ namespace TowerTanks.Scripts
 
         private void OnApplicationQuit()
         {
-            CampaignManager.Instance.EndCampaign();
+            Debug.Log(currentGameAnalytics.ToString());
         }
     }
 }
