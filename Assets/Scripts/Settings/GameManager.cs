f908c78bfc64bcc71126ba50865a00c46634323f
Ryan Pratt <cryanpratt@gmail.com>
2024-12-05 21:23:17 -0500

    Removed the Jetpack Refuel Nodule from all tank designs

--- Assets/Scripts/Settings/GameManager.cs
+++ Assets/Scripts/Settings/GameManager.cs
@@ -12,7 +12,7 @@ using UnityEngine.UI;
 
 namespace TowerTanks.Scripts
 {
-    public enum INTERACTABLE { Throttle, EnergyShield, Boiler, Refuel, Cannon, MachineGun, Mortar, Armor, ShopTerminal };
+    public enum INTERACTABLE { Throttle, EnergyShield, Boiler, Cannon, MachineGun, Mortar, Armor, ShopTerminal };
 
     public class GameManager : SerializedMonoBehaviour
     {
