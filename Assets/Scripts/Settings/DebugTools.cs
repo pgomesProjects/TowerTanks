c9515f6225f4f1aa8db70b1f912810c3ad456df8
P. Gomes <70992905+pgomesProjects@users.noreply.github.com>
2024-12-27 01:19:57 -0500

    Created Debug Joystick Tool

--- Assets/Scripts/Settings/DebugTools.cs
+++ Assets/Scripts/Settings/DebugTools.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace TowerTanks.Scripts.DebugTools
 {
@@ -9,6 +10,8 @@ namespace TowerTanks.Scripts.DebugTools
         [SerializeField, Tooltip("The main debug display.")] private CanvasGroup debugCanvasGroup;
         [SerializeField, Tooltip("The bug report screen.")] private BugReportForm bugReportScreenPrefab;
         [SerializeField, Tooltip("The command menu to spawn.")] private CheatInputField commandMenuPrefab;
+        [SerializeField, Tooltip("The joystick debug prefab.")] private DebugPlayerJoystick playerJoystickPrefab;
+        [SerializeField, Tooltip("The parent transform for the joystick debug prefabs.")] private RectTransform playerJoystickParent;
         private PlayerControlSystem playerControlSystem;
         private CheatInputField currentDebugMenu;
         private BugReportForm currentBugReportForm;
@@ -27,11 +30,13 @@ namespace TowerTanks.Scripts.DebugTools
         private void OnEnable()
         {
             playerControlSystem?.Enable();
+            GameManager.Instance.MultiplayerManager.OnPlayerConnected += SpawnPlayerJoystickDebug;
         }
 
         private void OnDisable()
         {
             playerControlSystem?.Disable();
+            GameManager.Instance.MultiplayerManager.OnPlayerConnected -= SpawnPlayerJoystickDebug;
         }
 
         public void ToggleDebugMode(bool isDebugMode)
@@ -96,6 +101,14 @@ namespace TowerTanks.Scripts.DebugTools
                 else
                     currentBugReportForm.gameObject.SetActive(true);
             }
+        }
+
+        private void SpawnPlayerJoystickDebug(PlayerInput playerInput)
+        {
+            DebugPlayerJoystick playerJoystick = Instantiate(playerJoystickPrefab, playerJoystickParent);
+            playerJoystick.name = "Player" + (playerInput.playerIndex + 1).ToString()  + "Joystick";
+            playerJoystick.transform.SetSiblingIndex(playerInput.playerIndex);
+            playerJoystick.LinkPlayerData(PlayerData.ToPlayerData(playerInput));
         }
     }
 }
